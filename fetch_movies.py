799aae0e4dec5f493402fdd32ca10a7fd8b3ac45
Uğurcan Demir <82281040+ugurcandemir@users.noreply.github.com>
2023-05-11 21:31:40 +0300

    Update fetch_movies.py

--- fetch_movies.py
+++ fetch_movies.py
@@ -2,16 +2,21 @@
 import tmdbv3api as mdb
 db = mdb.TMDb()
 db.api_key = 'API KEY'
+
 # get movies
 mov = db.Movie()
+
 # get popular movies
 movies = mov.popular()
+
 # get movie details
 movies[0].id
 movies[0].title
 movies[0].overview
 movies[0].vote_average
+
 # Get TV shows
+
 tvShow = db.TV()
 show = tvShow.search('Game of Thrones')
 show[0].name
