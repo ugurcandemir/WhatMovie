496fef5369cb3dc7f35531fb7e112f9c55c913a7
ugurcandemir <ugur.can.demir@hotmail.com>
2023-06-02 02:26:11 +0300

    api changes

--- data.py
+++ data.py
@@ -1,8 +1,7 @@
 
-
-
 import requests
+import config
 
-response = requests.get(url= 'https://api.themoviedb.org/3/movie/550?api_key=0a0a74a1434517653eb9680e6a14e6fb')
+response = requests.get(url= 'https://api.themoviedb.org/3/movie/550?api_key={}'.format(config.api_key))
 
 print(response.text)
