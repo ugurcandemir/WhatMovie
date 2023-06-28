2b2dc3b63f97065902d7e3d70f8c6222cd60233d
ugurcandemir <ugur.can.demir@hotmail.com>
2023-06-28 22:07:23 +0300

    28/06 Movie Data

--- model.py
+++ model.py
@@ -1,5 +1,11 @@
-##
-import sckitlearn
+## Import the libraries.
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 import pandas as pd
 
 # Get the data here.
