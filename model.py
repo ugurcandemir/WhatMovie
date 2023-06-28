93568782542cbb00c2cf214d8f2bc9510bd81c95
ugurcandemir <ugur.can.demir@hotmail.com>
2023-06-28 22:13:37 +0300

    reading movie data

--- model.py
+++ model.py
@@ -10,5 +10,25 @@ import pandas as pd
 
 # Get the data here.
 
+import json
+
+with open("movie.json"  , "r+") as f :
+    data = json.load(f)
+
+
+# Opening JSON file
+# f = open('data.json')
+  
+# returns JSON object as 
+# a dictionary
+
+  
+# Iterating through the json
+# list
+# for i in data['emp_details']:
+    # print(i)
+  
+# Closing file
+# f.close()d
 
 # Fit the model.
