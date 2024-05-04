69814f4a76d24fb70dc845ba9de7c65a72ceac6e
ugurcandemir <ugur.can.demir@hotmail.com>
2024-05-04 17:06:20 +0300

    all the codin is done

--- test/test.py
+++ test/test.py
@@ -1,31 +1,15 @@
 import requests
+import json
 
 url = 'http://localhost:5000/predict'
-data = {
-    "adult": False,
-    "popularity": 7.2,
-    "revenue": 50000000,
-    "runtime": 120,
-    "vote_average": 7.5,
-    "vote_count": 1500,
-    "genres_1_name_Adventure": 0,
-    "genres_1_name_Animation": 0,
-    "genres_1_name_Comedy": 1,
-    "genres_1_name_Crime": 0,
-    "genres_1_name_Drama": 0,
-    "genres_1_name_Family": 0,
-    "genres_1_name_Fantasy": 0,
-    "genres_1_name_History": 0,
-    "genres_1_name_Horror": 0,
-    "genres_1_name_Music": 0,
-    "genres_1_name_Mystery": 0,
-    "genres_1_name_Romance": 0,
-    "genres_1_name_Science Fiction": 1,
-    "genres_1_name_Thriller": 0,
-    "genres_1_name_War": 0,
-    "genres_1_name_Western": 0
-}
+ 
+# Load input data from file
+with open('test/test_input.json', 'r') as file:
+    input_data = json.load(file)
 
-response = requests.post(url, json=data)
+response = requests.post(url, json=input_data)
 predictions = response.json()['predictions']
 print(predictions)
+
+
+
